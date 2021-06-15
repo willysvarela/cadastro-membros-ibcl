27c31efc05580fb6456803cd8042922cf59a5ba7
willysv <willysv@motorola.com>
2021-06-15 17:21:49 -0400

    feat: Initializing Pages and Creating MainCard Component

---
+++ components/MainCard/index.js
@@ -0,0 +1,12 @@
+import { Box } from '@chakra-ui/react';
+import React from 'react'
+
+const MainCard = () => {
+    return (
+        <Box>
+            
+        </Box>
+    )
+}
+
+export default MainCard;

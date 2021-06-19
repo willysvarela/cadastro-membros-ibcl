cb230d767cddc0aa238eff5d6daab8ad5ccc974d
willysv <willysv@motorola.com>
2021-06-19 12:56:16 -0400

    [front] Added StepBox to wrap the forms on each step.

--- pages/signup.js
+++ pages/signup.js
@@ -1,12 +1,16 @@
 import React, { useEffect, useState } from 'react'
 import PropTypes from 'prop-types'
 
+import { Box } from "@chakra-ui/react"
+
 import Container from "./../components/Container"
 import SignUpStep1 from "./../components/SignUpSteps/SignUpStep1";
 import SignUpStep2 from "./../components/SignUpSteps/SignUpStep2";
 import SignUpStep3 from "./../components/SignUpSteps/SignUpStep3";
+import SignUpRevisionStep from "./../components/SignUpSteps/SignUpRevisionStep";
 import CardHeader from '../components/CardHeader';
 import StepsBar from '../components/StepsBar';
+import StepBox from "../components/SignUpSteps/StepBox"
 
 import MemberContext from '../context/MemberContext';
 
@@ -16,8 +20,8 @@ const STEPS = [
     {stepIndex: 0, description: "Dados", status: STEP_STATUS.DOING },
     {stepIndex: 1, description: "Ministério", status: STEP_STATUS.HOLD },
     {stepIndex: 2, description: "Habilidades", status: STEP_STATUS.HOLD },
-    {stepIndex: 3, description: "Habilidades", status: STEP_STATUS.HOLD },
-    {stepIndex: 4, description: "Final", status: STEP_STATUS.HOLD }
+    {stepIndex: 3, description: "Revisão", status: STEP_STATUS.HOLD },
+    {stepIndex: 4, description: "Envio", status: STEP_STATUS.HOLD }
 ];
 
 const STEPS_INDEX = {
@@ -31,15 +35,14 @@ const STEPS_INDEX = {
 const SignUp = props => {
     const [steps, setSteps] = useState(STEPS);
     const [currentStep, setCurrentStep] = useState(0);
+    const [dataSteps, setDataSteps] = useState([]);
 
     const [memberData, setMemberData] = useState({});
 
     useEffect(() => {
-        console.log(currentStep);
     }, [currentStep]);
 
     const updateSteps = (newStepIndex, memberData = {}) => {
-        console.log({newStepIndex});
         const newSteps = steps.map(step => {
             const status = step.stepIndex < newStepIndex ? STEP_STATUS.DONE : step.stepIndex === newStepIndex ? STEP_STATUS.DOING : STEP_STATUS.HOLD;
             return {...step, status};
@@ -47,22 +50,53 @@ const SignUp = props => {
         setSteps(newSteps);
         setCurrentStep(newStepIndex);
         if (memberData)
-            updaterMemberData(memberData);
+            updateMemberData(memberData);
     }
 
-    const updaterMemberData = memberData => {
-        setMemberData(oldData => ({...oldData, memberData}));
+    const updateMemberData = memberData => {
+        console.log({ memberData })
+        setMemberData(oldData => ({...oldData, ...memberData}));
+    }
+
+    const submitMemberData = memberData => {
+        console.log({toSubmit: memberData});
     }
 
     return (
         <MemberContext.Provider>
-            <Container>
-                <CardHeader />
-                <StepsBar steps={steps} />
-                {currentStep === 0 && <SignUpStep1 onClickNext={(memberData) => updateSteps(STEPS_INDEX.TWO, memberData)}/>}
-                {currentStep === 1 && <SignUpStep2 onClickNext={(memberData) => updateSteps(STEPS_INDEX.THREE, memberData)} onClickBack={() => updateSteps(STEPS_INDEX.ONE)}/>}
-                {currentStep === 2 && <SignUpStep3 onClickNext={(memberData) => updateSteps(STEPS_INDEX.FOUR, memberData)} onClickBack={() => updateSteps(currentStep-1)}/>}
-            </Container>
+                <Container>
+                    <Box padding="30px">
+                        <CardHeader />
+                        <Box marginTop="30px"/>
+                        <StepsBar steps={steps} />
+                        <Box marginTop="30px"/>
+                        <StepBox
+                            onClickNext={(memberData) => updateSteps(STEPS_INDEX.TWO, memberData)}
+                            show={ currentStep===STEPS_INDEX.ONE }
+                            component={SignUpStep1} />
+
+                        <StepBox
+                            onClickNext={(memberData) => updateSteps(STEPS_INDEX.THREE, memberData)}
+                            onClickBack={() => updateSteps(STEPS_INDEX.ONE)}
+                            show={ currentStep===STEPS_INDEX.TWO }
+                            component={SignUpStep2} />
+
+                        <StepBox 
+                            onClickNext={(memberData) => updateSteps(STEPS_INDEX.FOUR, memberData)}
+                            onClickBack={() => updateSteps(currentStep-1)}
+                            show={ currentStep===STEPS_INDEX.THREE }
+                            component={SignUpStep3} />
+                        <StepBox
+                            onClickNext={() => submitMemberData(memberData)}
+                            onClickBack={() => updateSteps(currentStep-1)}
+                            show={ currentStep===STEPS_INDEX.FOUR }
+                            component={SignUpRevisionStep} 
+                            nextTitle="Enviar"
+                            memberData={{...memberData}}
+                        />
+
+                    </Box>
+                </Container>
         </MemberContext.Provider>
     )
 }

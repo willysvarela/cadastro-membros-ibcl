52c5676aa2a69d3b240b03f65207d11f337a2eb1
willysv <willysv@motorola.com>
2021-06-19 12:55:22 -0400

    [front] Feat: Added Revision Step to review all the information filled

--- utils/constants.js
+++ utils/constants.js
@@ -3,3 +3,29 @@ export const STEP_STATUS = {
     HOLD: "HOLD",
     DOING: "DOING"
 }
+
+export const FIELD_TITLE = {
+    name: "Nome",
+    birthDate: "Data de Nascimento",
+    fatherName: "Nome do Pai",
+    motherName: "Nome da Mãe",
+    address: "Endereço",
+    addressNumber: "Número",
+    district: "Bairro",
+    phone: "Telefone",
+    mail: "Email",
+    socialNetwork: "Rede Social",
+    gender: "Gênero",
+    ministries: "Ministérios",
+    intendedMinistries: "Ministérios Desejados",
+    courses: "Cursos já feitos",
+    otherCourses: "Outros Cursos já feitos",
+    howJoin: "Como se tornou membro da IBCL",
+    otherHowJoin: "Como se tornou membro da IBCL (Outro)",
+    favoriteReunion: "Departamento",
+    healthSkill: "Habílidades em Saúde",
+    teachSkills: "Habilidades em Educação",
+    socialSkills: "Habilidades em Social",
+    maintenanceSkills: "Habilidades em Manutenção",
+    otherSkills: "Outras Habilidades"
+}

37e1719a9cc366765cdb618cbeaab0ca2d453b29
Willys Varela <willys.varela@arctouch.com>
2022-12-28 17:16:08 -0400

    Back-End route to upload image to supabase storage

--- utils/storage.js
+++ utils/storage.js
@@ -1,22 +1,31 @@
-import { SupabaseStorageClient } from "@supabase/storage-js";
+import { StorageClient } from "@supabase/storage-js";
 
 const STORAGE_URL = process.env.STORAGE_URL;
 const SERVICE_KEY = process.env.SERVICE_KEY;
 const BUCKET_NAME = process.env.BUCKET_NAME;
 
-const storageClient = new SupabaseStorageClient(STORAGE_URL, {
+const storageClient = new StorageClient(STORAGE_URL, {
   apikey: SERVICE_KEY,
   Authorization: `Bearer ${SERVICE_KEY}`,
 });
 
-const uploadFile = async (fileBody) => {
+const saveOnStorage = async (fileData, fileBody) => {
   const { data, error } = await storageClient
     .from(BUCKET_NAME)
-    .upload("", fileBody);
+    .upload(fileData.originalFilename, fileBody, {
+      cacheControl: 3600,
+      contentType: fileData.mimetype,
+      upsert: true,
+    });
   if (error) {
     return error;
   }
   return data;
 };
 
-export { storageClient, uploadFile };
+const getPublicUrl = (filePath) => {
+  const { data } = storageClient.from(BUCKET_NAME).getPublicUrl(filePath);
+  return data?.publicUrl;
+};
+
+export { storageClient, saveOnStorage, getPublicUrl };

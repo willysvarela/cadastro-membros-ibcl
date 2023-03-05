c42ff5ef5df3e958fe90dcc9a12c9522e1a74a9e
Willys Varela <willys.varela@akqa.com>
2023-03-05 15:03:39 -0400

    Add tailwindcss and daisyui libs

---
+++ tailwind.config.js
@@ -0,0 +1,8 @@
+module.exports = {
+  content: [
+    './pages/**/*.{js,ts,jsx,tsx}',
+    './components/**/*.{js,ts,jsx,tsx}'
+  ],
+  // eslint-disable-next-line global-require
+  plugins: [require('daisyui')]
+};
